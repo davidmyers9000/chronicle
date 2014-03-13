028c2664cbbdfb3614090db3b6504270ef966226
David Myers <davidmyers9000@gmail.com>
2014-03-13 11:37:36 -0400

    updated video comment paths, added user view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :set_video
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
-  before_action :set_video, only: [:index, :new, :create]
 
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    # @comments = Comment.all
+    redirect_to @video
   end
 
   # GET /comments/1
@@ -30,7 +31,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @video, notice: 'Comment was successfully created.' }
         format.json { render action: 'show', status: :created, location: @comment }
       else
         format.html { render action: 'new' }
@@ -44,7 +45,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
+        format.html { redirect_to @video, notice: 'Comment was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -58,23 +59,23 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url }
+      format.html { redirect_to video_url(@video) }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
+    def set_video
+      @video = Video.find(params[:video_id])
     end
 
-    def set_video
-      @video = Video.find(params[:id])
+    def set_comment
+      @comment = @video.comments.find(params[:id])
     end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(@video, :comment).permit(:source, :text, :user_id, :video_id)
+      params.require(:comment).permit(:source, :text)
     end
 end
